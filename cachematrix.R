51bc1a5c0e79de7edbc3a2895b263ae3c5228ba3
jasontincher <jason.tincher@icloud.com>
2016-03-20 12:41:17 -0400

    cachSolve function comments updated.

--- cachematrix.R
+++ cachematrix.R
@@ -23,7 +23,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function returns the "inverse matrix" of the object returned by the 
+## makeCacheMatrix function above.
 
 cacheSolve <- function(x, ...) {
         
