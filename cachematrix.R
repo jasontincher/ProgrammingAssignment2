3a1510e1c3022f79ae51d32656e8b08e375498d3
jasontincher <jason.tincher@icloud.com>
2016-03-20 12:09:02 -0400

    Modifications made to sample code
    Changes were made to the Coursera sample code that demonstrate R’s
    ability to cache a value.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## It is often useful to store a value for later us in a program.  R's lexical
+## scoping design allows functions/programs to store values within different 
+## environment levels. The two fucntions below demonstrate this capability.
 
-## Write a short comment describing this function
+## Below are two functions that are used to create a special object that stores
+## a matrix and cache's its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        invMatrix <- NULL
+        set <- function(y) {
+                x <<- y
+                invMatrix <<- NULL
+        }
+  
+        get <- function() x
+        setInvMatrix <- function(inverse) invMatrix <<- inverse
+        getInvMatrix <- function() invMatrix
+        list(set = set, 
+             get = get, 
+             setInvMatrix = setInvMatrix, 
+             getInvMatrix = getInvMatrix)  
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+        
         ## Return a matrix that is the inverse of 'x'
+        invMatrix <- x$getInvMatrix()
+        if(!is.null(invMatrix)) {
+                message("getting cached data")
+                return(invMatrix)
+        }
+        matrix <- x$get()
+        invMatrix <- solve(matrix, ...)
+        x$setInvMatrix(invMatrix)
+        invMatrix
 }
